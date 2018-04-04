4c6b571f0b13f127c9521d15d388bbf7138b392c
Scott Foster <thescottfoster@gmail.com>
2018-04-03 20:12:53 -0500

    added resume seeds

---
+++ seed.js
@@ -0,0 +1,71 @@
+let name = "Scott Foster";
+let skills = ["JavaScript", "HTML5", "CSS3", "Mongo", "Express", "React", "Node.js", "Git"];
+let urls = ["about.me/thescottfoster", "linkedin.com/in/thescottfoster", "github.com/mindonshuffle"];
+let email = ["thescottfoster@gmail.com"];
+let location = ["Chicago, IL"];
+let phoneNumber = "815.901.7884";
+
+let bio = "Junior Web Developer seeking to leverage my lifelong passion for technology and established skills at problem-solving towards tackling new challenges and building new experiences."
+
+let projects = [
+    {
+        name: "res.create",
+        url: "res-create.herokuapp.com",
+        role: "front-end developer",
+        details: ["Web app for storing CV/resume information and creating clean, printable designs", "Created Express-based API routes for communication between mySQL database and Materialize-based front-end", "Designed visual layout wireframes; implemented in HTML, CSS, and Materialize CSS framework"],
+    },
+    {
+        name: "HabiTrail",
+        url: "habitrail.herokuapp.com",
+        role: "sole developer",
+        details: ["MERN-stack app allowing users to track their progress and train positive life habits", "Developed underlying database schema with MongoDB and Materialize", "Built React-based Single Page Application front-end conforming to Material Design specifications"],
+    },
+    {
+        name: "Pinbox",
+        url: "mindonshuffle.github.io/Pinbox/",
+        role: "back-end developer",
+        details: ["Firebase-based app to search, save, and annotate web-development learning resources", "Built communications between front-end design and Firebase database and authentication", "Integrated Google Custom Search API for topic-specific search results"],
+    },    
+];
+
+let employment = [
+    {
+        employerName: "Freelance Photography",
+        employmentDates: "Jun 2014 — Present",
+        title: "Wedding, Event, and Portrait Photographer",
+        details: null,
+    },
+    {
+        employerName: "Resource Point of Sale",
+        employmentDates: "Nov 2016 — Jul 2017",
+        title: "Bench Technician",
+        details: ["Triage, diagnose, and repair diverse Point-of-Sale and PC systems", "Perform network-based software / OS recovery and reinstall operations", "Implement testing and documentation protocols for client hardware and software"],
+    },
+    {
+        employerName: "Verizon Wireless",
+        employmentDates: "Dec 2009 — Apr 2015",
+        title: "Technical Support Coordinator",
+        details: ["Assist customers solving various problems with cell phone, netbook, tablet, and mobile broadband service, including connectivity issues, configuration and activation, and application support", "Provide internal training and support for new products, services, and technologies"],
+    },
+    {
+        employerName: "The Foster & Buick Law Group, LLC",
+        employmentDates: "Jun 2006 — Aug 2010",
+        title: "IT Coordinator",
+        details: null,
+    },    
+];
+
+let education = [
+    {
+        school: "Northwestern University",
+        location: "Chicago, IL",
+        program: "Full-Stack Web Development Boot Camp",
+        completionDate: "2017",
+    },
+    {
+        school: "Northern Illinois University",
+        location: "Dekalb, IL",
+        program: "B.A. Philosophy",
+        completionDate: "2007",
+    },
+];
